cea7c6de5c44fd773ee4904baf851e10d21a7a3a
Thomas Backlund <bashlund@universe.ai>
2023-08-08 23:51:23 +0200

    Update integration test Chat to use SimpleChat

--- test/integration/chat/Chat.ts
+++ test/integration/chat/Chat.ts
@@ -1,23 +1,17 @@
 import { strict as assert } from "assert";
 
 import {
-    P2PClient,
-    FetchResponse,
-    ReadBlobResponse,
-    DATANODE_TYPE,
     DataInterface,
-    NodeInterface,
-    Status,
-    StorageUtil,
-    App,
-    SignatureOffloader,
-    SignatureOffloaderInterface,
-    HandshakeFactoryFactoryInterface,
-    CreateHandshakeFactoryFactory,
+    //SignatureOffloader,
+    //CreateHandshakeFactoryFactory,
     sleep,
     Hash,
     BlobEvent,
     ParseUtil,
+    SimpleChat,
+    TransformerCache,
+    TransformerItem,
+    AbstractStreamReader,
 } from "../../../";
 
 import {
@@ -31,7 +25,6 @@ import {
 } from "../../../src/util/JSONUtil";
 
 import {
-    LogLevel,
     PocketConsole,
 } from "pocket-console";
 
@@ -39,103 +32,19 @@ const BLOB_DATA = Buffer.concat([Buffer.alloc(1024 * 20).fill(0xa0),
     Buffer.alloc(1024 * 20).fill(0xb0),
     Buffer.alloc(1024 * 20).fill(0xc0)]);
 
-class Chat extends App {
-    protected console: any;
-    protected onMessage: Function;
-    protected onBlob?: Function;
-
-    constructor(publicKey: Buffer, signatureOffloader: SignatureOffloaderInterface,
-        handshakeFactoryFactory: HandshakeFactoryFactoryInterface,
-        console: any, onMessage: Function, onBlob?: Function) {
-
-        super(publicKey, signatureOffloader, handshakeFactoryFactory);
-
-        this.console = console;
-        this.onMessage = onMessage;
-        this.onBlob = onBlob;
+class StreamReader extends AbstractStreamReader {
+    constructor(protected data: Buffer) {
+        super(0n);
     }
 
-    protected onBlobHandler = (blobEvent: BlobEvent) => {
-        if (this.onBlob) {
-            this.onBlob(blobEvent);
+    protected async read() {
+        if (this.pos === 0n) {
+            this.buffered.push({size: BigInt(this.data.length), data: this.data, pos: this.pos});
+            this.pos = BigInt(this.data.length);
         }
-    };
-
-    /**
-     * @throws on signing failure.
-     */
-    public async sendMessage(message: string, blobData?: Buffer) {
-        const publicKey = this.getPublicKey();
-
-        if (!publicKey) {
-            throw new Error("App not configured, cannot send message");
+        else {
+            this.buffered.push({size: BigInt(this.data.length), data: Buffer.alloc(0), pos: this.pos});
         }
-
-        let blobLength: bigint | undefined;
-        let blobHash: Buffer | undefined;
-
-        if (blobData) {
-            blobLength = BigInt(blobData.length);
-            blobHash = Hash(blobData);
-        }
-
-        const nodeCerts = this.getNodeCerts();
-
-        const dataNode = await this.nodeUtil.createDataNode({owner: publicKey, isLicensed: true, data: Buffer.from(message), blobHash, blobLength}, publicKey, undefined, nodeCerts);
-        const licenseNode = await this.nodeUtil.createLicenseNode({nodeId1: dataNode.getId1(), owner: publicKey, extensions: 2, targetPublicKey: publicKey}, publicKey, undefined, nodeCerts);
-
-        const nodes = [dataNode, licenseNode];
-
-        this.storeNodes(nodes).then( (storedId1s: Buffer[]) => {
-            if (storedId1s.length !== nodes.length) {
-                throw new Error(`Expected to store ${nodes.length} nodes`);
-            }
-
-            if (blobData) {
-                // Upload blob with a delay to test that the trigger works.
-                setTimeout( () => {
-
-                    this.storeBlob(dataNode.getId1()!, blobData);
-
-                }, 500);
-            }
-        }).catch( e => {
-            console.error("Could not store nodes", e);
-        });
-    }
-
-    public setupAppFetch() {
-        if (!this.isConnected()) {
-            throw new Error("Not connected");
-        }
-
-        const fetchRequest = StorageUtil.CreateFetchRequest({
-            query: {
-                parentId: Buffer.alloc(32).fill(0),
-                triggerNodeId: Buffer.alloc(32).fill(0),
-                discardRoot: true,
-                match: [
-                    {nodeType: DATANODE_TYPE},
-                ],
-            },
-            transform: {
-                algos: [2],
-                cacheId: 1,
-                tail: 30,
-                includeDeleted: true,
-            },
-        });
-
-        this.fetchNodes(fetchRequest, (nodes: NodeInterface[]) => {
-            nodes.forEach( (node: NodeInterface) => {
-                if (node.getType().equals(DATANODE_TYPE)) {
-                    this.onMessage((node as DataInterface).getData()?.toString());
-                }
-            });
-        }).catch( e => {
-            console.error("Unexpected error occured when fetching nodes", e);
-            throw e;
-        });
     }
 }
 
@@ -291,12 +200,29 @@ async function main() {
 
     const consoleServer = PocketConsole({module: "Server"});
 
-    const chatServer = new Chat(keyPair1.publicKey, signatureOffloader1, handshakeFactoryFactory1, consoleServer, (message: string) => {
-        consoleServer.info(message);
-        checkToQuit();
-    }, async (blobEvent: BlobEvent) => {
+    const chatServer = new SimpleChat(keyPair1.publicKey, signatureOffloader1, handshakeFactoryFactory1);
+
+    chatServer.onConnectionError( (e) => {
+        consoleMain.error("Connection error in server", e);
+        process.exit(1);
+    });
+
+    chatServer.onChatReady( (cache: TransformerCache) => {
+        cache.onAdd( (item: TransformerItem) => {
+            const message = (item.node as DataInterface).getData()?.toString();
+            consoleServer.info(message);
+            checkToQuit();
+        });
+
+        // Here we send as soon as Storage is connected, peer might or might
+        // not be connected yet.
+        consoleMain.info("Storage connected, send message from Server side");
+        chatServer.sendChat("Hello from Server");
+    });
+
+    chatServer.onBlob( async (blobEvent: BlobEvent) => {
         if (blobEvent.nodeId1 && !blobEvent.error) {
-            const blobData = await chatServer.readBlob(blobEvent.nodeId1!);
+            const blobData = await chatServer.getLib()!.readBlob(blobEvent.nodeId1!);
 
             if (blobResolve) {
                 blobResolve(blobData);
@@ -304,32 +230,11 @@ async function main() {
         }
     });
 
-    chatServer.onConnectionError( (e) => {
-        consoleMain.error("Connection error in server", e);
-        process.exit(1);
-    });
-
-    chatServer.onStorageConnect( () => {
-        // Here we send as soon as Storage is connected, peer might or might
-        // not be connected yet.
-        consoleMain.info("Storage connected, send message from Server side");
-        chatServer.sendMessage("Hello from Server");
-    });
-
     chatServer.onConnectionConnect( () => {
         consoleMain.info("Connection connected to server");
     });
 
-    let [status, err] = await chatServer.parseConfig(serverConfig);
-
-    if (!status) {
-        consoleMain.error(`Could not parse config: ${err}.`);
-        process.exit(1);
-    }
-
-    chatServer.onStorageConnect( () => {
-        chatServer.setupAppFetch();
-    });
+    await chatServer.init(serverConfig);
 
     chatServer.onStop( () => {
         signatureOffloader1.close();
@@ -342,32 +247,31 @@ async function main() {
     consoleMain.info("Init Chat Client");
     const consoleClient = PocketConsole({module: "Client"});
 
-    const chatClient = new Chat(keyPair2.publicKey, signatureOffloader2, handshakeFactoryFactory2, consoleClient, (message: string) => {
-        consoleClient.info(message);
-        checkToQuit();
-    });
+
+    const chatClient = new SimpleChat(keyPair2.publicKey, signatureOffloader2, handshakeFactoryFactory2);
 
     chatClient.onConnectionError( (e) => {
         consoleMain.error("Connection error in client", e);
         process.exit(1);
     });
 
+    chatClient.onChatReady( (cache: TransformerCache) => {
+        cache.onChange( (item: TransformerItem, changeType: string) => {
+            const message = (item.node as DataInterface).getData()?.toString();
+            consoleClient.info(message);
+            checkToQuit();
+        });
+    });
+
     chatClient.onConnectionConnect( () => {
-        // Here we send when peer (server) is connected.
         consoleMain.info("Connection connected to client, send message from Client side");
-        chatClient.sendMessage("Hello from Client", BLOB_DATA);
+        const blobLength = BigInt(BLOB_DATA.length);
+        const blobHash = Hash(BLOB_DATA);
+        const streamReader = new StreamReader(BLOB_DATA);
+        chatClient.sendAttachment("Hello from Client with attachment", blobHash, blobLength, streamReader);
     });
 
-    [status, err] = await chatClient.parseConfig(clientConfig);
-
-    if (!status) {
-        consoleMain.error(`Could not parse config: ${err}.`);
-        process.exit(1);
-    }
-
-    chatClient.onStorageConnect( () => {
-        chatClient.setupAppFetch();
-    });
+    await chatClient.init(clientConfig);
 
     chatClient.onStop( () => {
         signatureOffloader2.close();
@@ -378,7 +282,7 @@ async function main() {
     const blobData = await blobPromise as Buffer;
     assert(blobData.equals(BLOB_DATA));
 
-    consoleServer.info("Downloaded blob");
+    consoleServer.aced("Transferred blob successfully");
     checkToQuit();
 }
 
