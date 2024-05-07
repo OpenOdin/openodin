b9633374e16b7350252d5510afa51dce6af6d288
Maicon Filippsen <filippsen@users.noreply.github.com>
2024-05-07 13:02:08 -0300

    Replace browserify with webpack
    + Add buffer as development dependency of dependencies
    
    + Add vm-browserify as dependency to libsodium
    
    + Add new webpack configuration file for building signature offloader
    
    + Add new webpack configuration file for building crdt worker

---
+++ webpack.signatureOffloader-config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "production",
+    entry: "./build/src/signatureoffloader/signatureOffloader-worker.js",
+    output: {
+        path: path.resolve(__dirname, "build/src/signatureoffloader"),
+        filename: "./signatureOffloader-worker-browser.js",
+        pathinfo: true,
+        library: {
+            type: "umd",
+            name: "add",
+        }
+    },
+    resolve: {
+        alias: {
+            "fs": false,
+            "http": false,
+            "os": false,
+            "net": false,
+            "tls": false,
+            "https": false,
+            "sqlite3": false,
+            "postgresql-client": false
+        },
+        extensions: [".tsx", ".ts", ".js"],
+        fallback: {
+            crypto: require.resolve("crypto-browserify"),
+            path:   require.resolve("path-browserify"),
+            stream: require.resolve("stream-browserify"),
+            vm:     require.resolve("vm-browserify"),
+            buffer: require.resolve("buffer/")
+        }
+    },
+    module: {
+          noParse: /\/node_modules\/process\//,
+    },
+    plugins: [
+          new webpack.ProvidePlugin({
+              process: "process/browser.js",
+          }),
+          new webpack.DefinePlugin({
+              process: {
+                  env: {
+                      "process.env.NODE_ENV" : JSON.stringify("production")
+                  }
+              }
+        }),
+        new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ]
+};
